79c51189f4bebf5325025654044ce39b56f797d3
Oscar Stiven Ciro Alvarez <stivenciroalvarez@gmail.com>
2026-01-06 16:02:10 +0000

    ´´<anonymous>@rsc://React/Server/file:///home/user/studio/.next/server/c

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-const geminiPro = googleAI.model('gemini-pro');
+const geminiPro = googleAI.model('gemini-1.0-pro');
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
